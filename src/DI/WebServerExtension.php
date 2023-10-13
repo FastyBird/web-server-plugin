7956e696393251b3cacf82442f8c00ca12d66f55
GitHub <action@github.com>
2023-10-13 08:39:31 +0000

    [sonoff-connector] Refactoring to new structure (#148)

--- src/DI/WebServerExtension.php
+++ src/DI/WebServerExtension.php
@@ -47,7 +47,6 @@ class WebServerExtension extends DI\CompilerExtension
 		string $extensionName = self::NAME,
 	): void
 	{
-		// @phpstan-ignore-next-line
 		$config->onCompile[] = static function (
 			BootstrapBoot\Configurator $config,
 			DI\Compiler $compiler,
